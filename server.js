c2e5083d4abf8c5d3d370dfe9a58d99c87a83423
Joshua Bohde <bohdecoded@gmail.com>
2022-10-30 13:17:02 -0400

    install sequelize and configure db

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require('path');
 
+const sequelize = require('./db/config');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -16,6 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+const force = process.env.FORCE_SYNC === 'true';
+
+sequelize.sync({ force }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+})
