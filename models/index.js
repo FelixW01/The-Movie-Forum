6628e199e91167236f8ad9db90eebc94a0531f11
[Ashley Braxton] <[ashley.braxton25@gmail.com]>
2023-06-26 19:58:56 -0400

    created movie model

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const User = require('./User');
 const Post = require('./post');
 const Comment = require('./comment');
+const Movie = require('./movie');
 
 module.exports = {
   User, 
   Post,
-  Comment
+  Comment,
+  Movie
 };
---
+++ models/movie.js
@@ -0,0 +1,29 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../db/config');
+
+class Movie extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+          },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        poster: {
+            type: DataTypes.BLOB,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        modelName: 'movie'    
+    }
+);
+
+module.exports = Movie;
