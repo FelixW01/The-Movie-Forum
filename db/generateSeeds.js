c51102978e17a03001cd2ccb20fed4f8bf5f98d7
Herb Sneed <hmsneed@gmail.com>
2023-06-28 18:44:22 -0400

    more seed work

--- db/generateSeeds.js
+++ db/generateSeeds.js
@@ -33,13 +33,13 @@ VALUES (001, "Elemental", "${elementalBiDa}"),
        (003, "The Little Mermain", "${littleMermaidBiDa}"),
        (004, "The Flash", "${theFlashMoviePosterBiDa}");
 
-INSERT INTO Comment (id, user, content, parent_post)
+INSERT INTO Comment (id, username, content, parent_post)
 VALUES (000, "alphazeta", "The Flash", "This is the best movies ever"),
        (001, "funrunner79", "Elemental", "My child loved this film"),
        (002, "askmenot31", "No Hard Feelings", "I tried to laugh... I tried."),
        (003, "almostwinning24", "The Little Mermaid", "My daughter enjoyed it, so I did too!");
 
-INSERT INTO Post (id, title, user, description, content, img)
+INSERT INTO Post (id, title, username, description, content, img)
 VALUES (001, "Elemental", "funrunner79", "Too Funny", "My child loved this film", "${elementalBiDa}"),
        (002, "No Hard Feelings", "askmenot31", "Not Really,", "I tried to laugh... I tried.", "${noHardFeelingsBiDa}"),
        (003, "The Little Mermaid", "almostwinning24", "Happy Child", "littleMermaidBiDa", "${littleMermaidBiDa}"),
@@ -97,25 +97,25 @@ const generateSeed = async function () {
     await Comment.bulkCreate([
       {
         id: 0,
-        user: 'alphazeta',
+        username: 'alphazeta',
         content: 'The Flash',
         parent_post: 'This is the best movie ever',
       },
       {
         id: 1,
-        user: 'funrunner79',
+        username: 'funrunner79',
         content: 'Elemental',
         parent_post: 'My child loved this film',
       },
       {
         id: 2,
-        user: 'askmenot31',
+        username: 'askmenot31',
         content: 'No Hard Feelings',
         parent_post: 'I tried to laugh... I tried.',
       },
       {
         id: 3,
-        user: 'almostwinning24',
+        username: 'almostwinning24',
         content: 'The Little Mermaid',
         parent_post: 'My daughter enjoyed it, so I did too!',
       },
@@ -126,7 +126,7 @@ const generateSeed = async function () {
       {
         id: 1,
         title: 'Elemental',
-        user: 'funrunner79',
+        username: 'funrunner79',
         description: 'Too Funny',
         content: 'My child loved this film',
         img: elementalBiDa,
@@ -134,7 +134,7 @@ const generateSeed = async function () {
       {
         id: 2,
         title: 'No Hard Feelings',
-        user: 'askmenot31',
+        username: 'askmenot31',
         description: 'Not Really',
         content: 'I tried to laugh... I tried.',
         img: noHardFeelingsBiDa,
@@ -142,7 +142,7 @@ const generateSeed = async function () {
       {
         id: 3,
         title: 'The Little Mermaid',
-        user: 'almostwinning24',
+        username: 'almostwinning24',
         description: 'Happy Child',
         content: 'littleMermaidBiDa',
         img: littleMermaidBiDa,
@@ -150,7 +150,7 @@ const generateSeed = async function () {
       {
         id: 4,
         title: 'The Flash',
-        user: 'alphazeta',
+        username: 'alphazeta',
         description: "I'm a fan again",
         content: 'This is the best movies ever',
         img: theFlashMoviePosterBiDa,
--- db/seeds.sql
+++ db/seeds.sql
@@ -10,13 +10,13 @@ VALUES (001, "Elemental"),
        (003, "The Little Mermain"),
        (004, "The Flash");
 
-INSERT INTO Comment (id, user, content, parent_post)
+INSERT INTO Comment (id, username, content, parent_post)
 VALUES (000, "alphazeta", "The Flash", "This is the best movies ever"),
        (001, "funrunner79", "Elemental", "My child loved this film"),
        (002, "askmenot31", "No Hard Feelings", "I tried to laugh... I tried."),
        (003, "almostwinning24", "The Little Mermaid", "My daughter enjoyed it, so I did too!");
 
-INSERT INTO Post (id, title, user, description, content)
+INSERT INTO Post (id, title, username, description, content)
 VALUES (001, "Elemental", "funrunner79", "Too Funny", "My child loved this film"),
        (002, "No Hard Feelings", "askmenot31", "Not Really,", "I tried to laugh... I tried."),
        (003, "The Little Mermaid", "almostwinning24", "Happy Child", "littleMermaidBiDa"),
