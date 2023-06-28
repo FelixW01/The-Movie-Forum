05f62565ad2b35074feed7ea0c03d1ee4ed8cd99
Herb Sneed <hmsneed@gmail.com>
2023-06-28 19:57:04 -0400

    updating seed data

--- db/generateSeeds.js
+++ db/generateSeeds.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 
-// type: DataTypes.BLOB('long'),
-
 const imageBuffer1 = fs.readFileSync('./db/Movie_Posters/elemental-md-web.jpg');
 const elementalBiDa = imageBuffer1.toString('base64');
 
@@ -33,17 +31,18 @@ VALUES (1, "Elemental", "${elementalBiDa}"),
        (3, "The Little Mermain", "${littleMermaidBiDa}"),
        (4, "The Flash", "${theFlashMoviePosterBiDa}");
 
-INSERT INTO Comment (id, username, content)
-VALUES (1, "alphazeta", "The Flash", "This is the best movies ever"),
-       (2, "funrunner79", "Elemental", "My child loved this film"),
-       (3, "askmenot31", "No Hard Feelings", "I tried to laugh... I tried."),
-       (4, "almostwinning24", "The Little Mermaid", "My daughter enjoyed it, so I did too!");
+INSERT INTO Comment (id, userId, username, content)
+VALUES (1, 1, "alphazeta", "This is the best movies ever"),
+       (2, 2, "funrunner79", "My child loved this film"),
+       (3, 3, "askmenot31", "I tried to laugh... I tried."),
+       (4, 4, "almostwinning24", "My daughter enjoyed it, so I did too!");
 
-INSERT INTO Post (id, title, username, description, content, img)
-VALUES (001, "Elemental", "funrunner79", "Too Funny", "My child loved this film", "${elementalBiDa}"),
-       (002, "No Hard Feelings", "askmenot31", "Not Really,", "I tried to laugh... I tried.", "${noHardFeelingsBiDa}"),
-       (003, "The Little Mermaid", "almostwinning24", "Happy Child", "littleMermaidBiDa", "${littleMermaidBiDa}"),
-       (004, "The Flash", "alphazeta", "I'm a fan again", "This is the best movies ever", "${theFlashMoviePosterBiDa}");
+INSERT INTO Post (id, title, userId, description, content, img)
+VALUES (1, "Elemental", 2, "Too Funny", "My child loved this film", 
+       "${elementalBiDa}"),
+       (2, "No Hard Feelings", 3, "Not Really,", "I tried to laugh... I tried.", "${noHardFeelingsBiDa}"),
+       (3, "The Little Mermaid", 4, "Happy Child", "littleMermaidBiDa", "${littleMermaidBiDa}"),
+       (4, "The Flash", 1,"I'm a fan again", "This is the best movies ever", "${theFlashMoviePosterBiDa}");
        `;
 
 const generateSeed = async function () {
@@ -56,25 +55,25 @@ const generateSeed = async function () {
     // Insert users
     await User.bulkCreate([
       {
-        id: 0,
+        id: 1,
         username: 'alphazeta',
         email: 'rsmith@gmail.com',
         password: '12345678',
       },
       {
-        id: 1,
+        id: 2,
         username: 'funrunner79',
         email: 'jjones@gmail.com',
         password: '12345678',
       },
       {
-        id: 2,
+        id: 3,
         username: 'askmenot31',
         email: 'jalcon33@gmail.com',
         password: '12345678',
       },
       {
-        id: 3,
+        id: 4,
         username: 'almostwinning24',
         email: 'rward@gmail.com',
         password: '12345678',
@@ -83,31 +82,51 @@ const generateSeed = async function () {
 
     // Insert movies
     await Movie.bulkCreate([
-      { id: 1, title: 'Elemental', poster: elementalBiDa },
-      { id: 2, title: 'No Hard Feelings', poster: noHardFeelingsBiDa },
-      { id: 3, title: 'The Little Mermaid', poster: littleMermaidBiDa },
-      { id: 4, title: 'The Flash', poster: theFlashMoviePosterBiDa },
+      { 
+        id: 1,
+        title: 'Elemental', 
+        poster: elementalBiDa 
+      },
+      { 
+        id: 2, 
+        title: 'No Hard Feelings', 
+        poster: noHardFeelingsBiDa 
+      },
+      { 
+        id: 3,
+        title: 'The Little Mermaid',
+        poster: littleMermaidBiDa 
+      },
+      { 
+        id: 4, 
+        title: 'The Flash', 
+        poster: theFlashMoviePosterBiDa 
+      },
     ]);
 
     // Insert comments
     await Comment.bulkCreate([
       {
         id: 1,
+        userId: 1,
         username: 'alphazeta',
         content: 'This is the best movie ever',
       },
       {
         id: 2,
+        userId: 2,
         username: 'funrunner79',
         content: 'My child loved this film',
       },
       {
         id: 3,
+        userId: 3,
         username: 'askmenot31',
         content: 'I tried to laugh... I tried.',
       },
       {
         id: 4,
+        userId: 4,
         username: 'almostwinning24',
         content: 'My daughter enjoyed it, so I did too!',
       },
@@ -118,34 +137,38 @@ const generateSeed = async function () {
       {
         id: 1,
         title: 'Elemental',
-        username: 'funrunner79',
+        userId: 2,
         description: 'Too Funny',
         content: 'My child loved this film',
         img: elementalBiDa,
+        movieId: 1,
       },
       {
         id: 2,
         title: 'No Hard Feelings',
-        username: 'askmenot31',
+        userId: 3,
         description: 'Not Really',
         content: 'I tried to laugh... I tried.',
         img: noHardFeelingsBiDa,
+        movieId: 2,
       },
       {
         id: 3,
         title: 'The Little Mermaid',
-        username: 'almostwinning24',
+        userId: 4,
         description: 'Happy Child',
         content: 'littleMermaidBiDa',
         img: littleMermaidBiDa,
+        movieId: 3,
       },
       {
         id: 4,
         title: 'The Flash',
-        username: 'alphazeta',
+        userId: 1,
         description: "I'm a fan again",
         content: 'This is the best movies ever',
         img: theFlashMoviePosterBiDa,
+        movieId: 4,
       },
     ]);
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -7,17 +7,17 @@ VALUES (1, "alphazeta", "rsmith@gmail.com", "12345678"),
 INSERT INTO Movie (id, title)
 VALUES (1, "Elemental"),
        (2, "No Hard Feelings"),
-       (3, "The Little Mermain"),
+       (3, "The Little Mermaid"),
        (4, "The Flash");
 
-INSERT INTO Comment (id, username, content)
-VALUES (1, "alphazeta", "This is the best movies ever"),
-       (2, "funrunner79", "My child loved this film"),
-       (3, "askmenot31", "I tried to laugh... I tried."),
-       (4, "almostwinning24", "My daughter enjoyed it, so I did too!");
+INSERT INTO Comment (id, userId, username, content)
+VALUES (1, 1, "alphazeta", "This is the best movies ever"),
+       (2, 2, "funrunner79", "My child loved this film"),
+       (3, 3, "askmenot31", "I tried to laugh... I tried."),
+       (4, 4, "almostwinning24", "My daughter enjoyed it, so I did too!");
 
-INSERT INTO Post (id, title, username, description, content)
-VALUES (1, "Elemental", "funrunner79", "Too Funny", "My child loved this film"),
-       (2, "No Hard Feelings", "askmenot31", "Not Really,", "I tried to laugh... I tried."),
-       (3, "The Little Mermaid", "almostwinning24", "Happy Child", "littleMermaidBiDa"),
-       (4, "The Flash", "alphazeta", "I'm a fan again", "This is the best movies ever");
+INSERT INTO Post (id, title, userId, description, content)
+VALUES (1, "Elemental", 2, "Too Funny", "My child loved this film"),
+       (2, "No Hard Feelings", 3, "Not Really,", "I tried to laugh... I tried."),
+       (3, "The Little Mermaid", 4, "Happy Child", "littleMermaidBiDa"),
+       (4, "The Flash", 1, "I'm a fan again", "This is the best movies ever");
