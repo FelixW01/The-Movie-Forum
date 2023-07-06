54a5d7648cdba7f8a5d560433bd4094d566b603a
Felix W <felixwillem01@yahoo.com>
2023-07-05 21:04:53 -0400

    got rid or seeds, fixed page errors

--- seeds/index.js
+++ seeds/index.js
@@ -7,31 +7,31 @@ const {
   Comment
 } = require('../models');
 
-const userSeeds = require('./users.json');
+// const userSeeds = require('./users.json');
 const movieSeeds = require('./api.json');
-const commentSeeds = require('./comment.json');
-const postSeeds = require('./post.json');
+// const commentSeeds = require('./comment.json');
+// const postSeeds = require('./post.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({
     force: true
   });
 
-  await User.bulkCreate(userSeeds, {
-    individualHooks: true,
-  });
+  // await User.bulkCreate(userSeeds, {
+  //   individualHooks: true,
+  // });
 
   await Movie.bulkCreate(movieSeeds, {
     individualHooks: true,
   });
 
-  await Post.bulkCreate(postSeeds, {
-    individualHooks: true,
-  });
+  // await Post.bulkCreate(postSeeds, {
+  //   individualHooks: true,
+  // });
 
-  await Comment.bulkCreate(commentSeeds, {
-    individualHooks: true,
-  });
+  // await Comment.bulkCreate(commentSeeds, {
+  //   individualHooks: true,
+  // });
 
 
   process.exit(0);
