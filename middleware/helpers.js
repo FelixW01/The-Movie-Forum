381fc7de0e0d2e8ad2ec2933f834698bc10c71cb
Joshua Bohde <bohdecoded@gmail.com>
2023-07-23 21:06:16 -0400

    refactor: return different date format

--- middleware/helpers.js
+++ middleware/helpers.js
@@ -1,7 +1,26 @@
 module.exports = {
-    formatDate: (date) => {
-        return `${new Date(date).getMonth() + 1}/${new Date(
-      date
-    ).getDate()}/${new Date(date).getFullYear()}`;
-    },
+  formatDate: (date) => {
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+
+    // Pad the day with leading zero if it's a single-digit number
+    const formattedDay = (day < 10 ? "0" : "") + day;
+
+    return `${month} ${formattedDay} ${year}`;
+  },
 };
